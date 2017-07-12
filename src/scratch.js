659f29a172d93151dfe373c832cab4fd62cb1231
zzp <i_zhongpeng@163.com>
2017-07-13 00:04:12 +0800

    modify scratch

--- src/scratch.js
+++ src/scratch.js
@@ -1,24 +1,38 @@
 import Rx from 'rxjs/Rx';
 import $ from 'jquery';
 
-import "babel-polyfill";
+// import "babel-polyfill";
 
 const source = new Rx.Observable(observer => {
     console.log('createing observable');
-    observer.next(1);
+    observer.next('hello');
+    observer.next('hello again');
+
+    
+    //
+    setTimeout(() => {
+        observer.next('timeout hello');
+        //
+        observer.complete();
+    }, 3000);
+
+    // 
+    observer.error(new Error('this is a error'));
 });
 
-source.subscribe(o => {
-    console.log(o);
+source
+.catch(err => Rx.Observable.of(err))
+.subscribe(o => {
+    console.log("arrow function -> " + o);
 }, err => {
-    console.log(err);
+    console.log('err arrow function -> ' + err);
 }, complete => {
     console.log('finished');
 });
 
-function* say(msg) {
-  console.log(msg)
-}
-var gen = say('hello world111');
-console.log(Object.prototype.toString.call(gen));
-gen.next();
+// function* say(msg) {
+//   console.log(msg)
+// }
+// var gen = say('hello world111');
+// console.log(Object.prototype.toString.call(gen));
+// gen.next();
